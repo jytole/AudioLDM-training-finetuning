78fcc75a8571c0d7acc927e8dc9659a2446f7fe1
jytole <kylerssmith@hotmail.com>
2025-02-25 18:29:30 -0600

    workfiles

---
+++ audioldm_train/train/testZipProcessor.py
@@ -0,0 +1,5 @@
+import latent_diffusion_api
+import os
+
+## Assumes this script is being run from the root folder of the repo
+latent_diffusion_api.handleDataUpload("./webapp/cache/HandPicked-Animal-Subset.zip")
---
+++ audioldm_train/train/test_api.py
@@ -0,0 +1,5 @@
+from audioldm_train.utilities.audioldm2_api import AudioLDM2APIObject
+
+if __name__ == "__main__":
+    apiInstance = AudioLDM2APIObject()
+    apiInstance.beginTrain()
