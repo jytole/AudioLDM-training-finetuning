9eb409a224c989e8dce84aaa26664296ef304908
jytole <kylerssmith@hotmail.com>
2025-03-26 10:56:29 -0500

    add logpath to echo

--- post_start.sh
+++ post_start.sh
@@ -5,6 +5,6 @@ cd /home/AudioLDM-training-finetuning
 conda activate audioldm_train
 poetry install
 python webapp/torchServer.py &
-echo "torch server started with logfiles"
+echo "torch server started with logfiles /home/AudioLDM-training-finetuning/webapp/torchServer*.log"
 gunicorn -c  webapp/gunicorn-conf.py webapp.flaskApp:app &
 echo "gunicorn started with logfiles /home/AudioLDM-training-finetuning/webapp/flask*Logs*.log"
