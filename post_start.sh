6e5f67b2b6e36117607d51ca00aa4310a32a13bc
jytole <kylerssmith@hotmail.com>
2025-03-26 09:20:25 -0500

    Fix scripts

--- post_start.sh
+++ post_start.sh
@@ -6,5 +6,5 @@ conda activate audioldm_train
 poetry install
 python webapp/torchServer.py &
 echo "torch server started with logfiles"
-gunicorn -c  gunicorn-conf.py webapp.flaskApp:app &
+gunicorn -c  webapp/gunicorn-conf.py webapp.flaskApp:app &
 echo "gunicorn started with logfiles /home/AudioLDM-training-finetuning/webapp/flask*Logs*.log"
--- startServer.sh
+++ startServer.sh
@@ -1,4 +1,4 @@
 python webapp/torchServer.py &
 echo "torch server started with logfiles"
-gunicorn -c  gunicorn-conf.py webapp.flaskApp:app &
+gunicorn -c  webapp/gunicorn-conf.py webapp.flaskApp:app &
 echo "gunicorn started with logfiles /home/AudioLDM-training-finetuning/webapp/flask*Logs*.log"
